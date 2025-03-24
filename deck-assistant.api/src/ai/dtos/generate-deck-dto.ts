35fa9c289d94fe58c3f850bcec04ba0d5ca9d0c6
SpectroXRL <spacespectrumdev@gmail.com>
2025-03-24 01:17:45 -0500

    Created Generete Deck DTO

---
+++ deck-assistant.api/src/ai/dtos/generate-deck-dto.ts
@@ -0,0 +1,16 @@
+export class GenerateDeckDTO{
+    companyName: string
+
+    industry: string
+
+    problemStatement: string
+
+    solution: string
+
+    businessModel: string
+
+    financials: string
+
+    teamInfo: string
+    
+}
